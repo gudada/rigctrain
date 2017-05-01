bf8e9d130c4a383c61ea8904254098cbde9b34ea
gudada <807471163@qq.com>
2017-05-01 18:42:38 +0800

    2017/5/1/gudada

---
+++ car.java
@@ -0,0 +1,18 @@
+public class car{
+String color;
+String size;
+String swich=off;
+
+public boolean Horm(){
+	return true;
+ }
+
+}
+public class index£û
+ public static void main(String[] args){
+	car honda=new car;
+	car BMW=new car;
+	honda.Horm();
+	BMW.Horm();
+}
+£ý
